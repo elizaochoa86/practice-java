9938954f2c0eec3d53ae0ec5b86ce81b6cf1048e
Eli Ochoa <elizaochoa86@gmail.com>
2021-03-16 11:58:37 -0700

    fibonacci problem (#3)
    Co-authored-by: Elizabeth Ochoa <elizabethochoa@Elizabeths-MacBook-Air.local>

---
+++ src/main/java/FibonacciNumersProblem.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class FibonacciNumersProblem {
+
+    /*
+     *  @see <a href="https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem?h_r=internal-search">Link to Picking Numbers Problem</a>
+     *
+     */
+
+    public static int fibonacci(int n) {
+        if(n == 0)
+            return 0;
+        else if(n == 1)
+            return 1;
+        else{
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+
+    public static void main(String args[]){
+
+        System.out.println(fibonacci(3));
+    }
+}
