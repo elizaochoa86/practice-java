775d82a72765177990c7d5b34b5fc9eb8757cc50
Eli Ochoa <elizaochoa86@gmail.com>
2021-03-23 11:17:31 -0700

    FInd Digits Problem Solution (#5)
    Co-authored-by: Elizabeth Ochoa <elizabethochoa@Elizabeths-MacBook-Air.local>

---
+++ src/main/java/FindDigitsProblem.java
@@ -0,0 +1,24 @@
+public class FindDigitsProblem {
+    /**
+     * @see <a href="https://www.hackerrank.com/challenges/find-digits/problem">
+     * Link to Find Digits Problem</a>
+     * @param n
+     * @return
+     */
+    // Complete the findDigits function below.
+    static int findDigits(int n) {
+        String digits = String.valueOf(n);
+        int count = 0;
+        for (int i = 0; i<digits.length(); i++){
+            int value = Integer.parseInt(String.valueOf(digits.charAt(i)));
+            if(value != 0 && n % value == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String args[]){
+        System.out.println(findDigits(10));
+    }
+}
